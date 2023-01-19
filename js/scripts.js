54cbf1ab342d217b224fbc3bc9ee9343c134e1e5
Fay Bauder <fay.bauder@gmail.com>
2023-01-18 20:19:31 -0800

    create new calls for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,28 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 //user interface logic
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
 
-window.alert(add(number1, number2));
+// window.alert("The sum is " + add(number1, number2) + "." + "The subtraction result is " + subtract(number1, number2) + "." + "The multiplication result is " + multiply(number1, number2) + "." + "The division result is " + divide(number1, number2) + ".")
+
+
+// window.alert("your addition result is " + add(number1, number2));
+// window.alert("your subtraction result is " + subtract(number1, number2));
+// window.alert("your multiplication result is " + multiply(number1, number2));
+// window.alert("your division result is " + divide(number1, number2));
+
+window.alert(parseInt(number1) + "+" + parseInt(number2) + "=" + add(number1, number2) + "." + parseInt(number1) + "-" + parseInt(number2) + "="  + subtract(number1, number2) + "." + parseInt(number1) + "*" + parseInt(number2) + "="  + multiply(number1, number2) + "." + parseInt(number1) + "/" + parseInt(number2) + "="  + divide(number1, number2) + ".")
